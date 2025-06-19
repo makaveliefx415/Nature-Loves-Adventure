98749807b8b6e80c73412fd4a06dae66a227814b
makaveliefx415 <makaveliefx474@gmail.com>
2025-06-19 17:57:14 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     <header
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
         isScrolled
-          ? "bg-white shadow-md py-3"
+          ? "bg-transparent shadow-lg py-5"
           : "bg-transparent py-5"
       }`}
     >
